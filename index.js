2c26a3cc51a4a5d6daafb8c9d3c561e56457c69e
HousseinBadra <88968898+HousseinBadra@users.noreply.github.com>
2022-04-23 05:18:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,83 @@
-import {getproducts} from '/products.js'
-let productsarr=getproducts()
+let productsarr=[
+  {
+    name:'Face mask',
+    price:'1.99$',
+    category:'protective equipment',
+    description:'Always wear a facemask for your safety',
+    url:'facemask.jpg'
+  }, 
+  {
+    name:'Guitar',
+    price:'69.99$',
+    category:'Musical intruments',
+    description:'A high quality guitar for a great musician',
+    url:'guitar.jpg'
+  },
+    {
+    name:'Harry Poter book',
+    price:'15.99$',
+    category:'Book',
+    description:'Reading a book never hurts specially Harry Poter',
+    url:'harrypoter.jpg'
+  },  {
+    name:'Honey bottle',
+    price:'24.99$',
+    category:'Food',
+    description:'Delicious honey that will make you eat your fingers',
+    url:'honey.jpg'
+  },  {
+    name:'Iphone 12',
+    price:'799.99$',
+    category:'Technology',
+    description:'Iphone 12 is the perfect phone for you',
+    url:'iphone12.jpg'
+  },  {
+    name:'Jalab sirup',
+    price:'9.99$',
+    category:'Food',
+    url:'jalab.png',
+    description:'The best drink for ramadan'
+  },  {
+    name:'The product book',
+    price:'14.99$',
+    category:'Book',
+    url:'productbook.jpg',
+    description:'The book to learn product managment'
+  },  {
+    name:'Smart TV',
+    price:'699.99$',
+    category:'Technology',
+    url:'smarttv.jpg',
+    description:'A big screen TV so you can have fun'
+  },  {
+    name:'Tablet',
+    price:'239.99$',
+    category:'Technology',
+    description:'A great tablet for a cheap price',
+    url:'tablet1.jpg'
+  },  {
+    name:'Toyota car',
+    price:'12999.99$',
+    category:'Car',
+    description:'The best car for you',
+    url:'toyota.jpg'
+  },
+    {
+    name:'Dates',
+    price:'4.99$',
+    category:'Food',
+    description:'Dates tastes the best',
+    url:'dates.jpg'
+  },  {
+    name:'Tesla madel X',
+    price:'29999.99$',
+    category:'Car',
+    description:'Drive safely in the best electrical car',
+    url:'tesla.jpg'
+  }
+  
+  ]
+
 
 const products=document.querySelector('.products')
 const filterbtn=document.querySelector('#filtertarget')
